53c6c738c5145a1939402fba802344119321a541
JimHaff <james.haff@generalassemb.ly>
2025-01-21 13:45:56 -0600

    added index

---
+++ controllers/fruits.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.delete('/:fruitId', async function(req, res){
+
+	const deletedFruit = await FruitModel.findByIdAndDelete(req.params.fruitId)
+
+	res.redirect('/')
+})
+
+
+router.get('/', async function(req, res){
+	// get all of the  from the db!
+
+	const allFruitDocs = await FruitModel.find({})
+	// console.log(allFruitDocs)
+
+	res.render('/index.ejs', {fruitDocs: allFruitDocs})
+})
+
+router.get('/new', function(req, res){
+
+	// render always looks in the views folder
+	// for the our ejs filesres.render('/new.ejs')!
+	res.render('/new.ejs')
+})
+
+
+// the new route must be defined before the show, because params are catch alls
+router.get('/:fruitId', async function(req, res){
+	console.log(req.params.fruitId, " <- req.params.fruitId")
+
+	// using the id from the request, 
+	// tell the model to go find that specific fruit from the database!
+	const fruitDoc = await FruitModel.findById(req.params.fruitId)
+	console.log(fruitDoc)
+
+	res.render('/show.ejs', {fruitDoc: fruitDoc})
+})
+
+
+
+router.post('/', async function(req, res){
+
+	// to access the contents of the form
+	console.log(req.body, " <- body of our request")
+	if(req.body.isReadyToEat === 'on'){
+		req.body.isReadyToEat = true
+	} else {
+		req.body.isReadyToEat = false
+	}
+
+	// one line of if/else
+	// req.body.isReadyToEat = !!req.body.isReadyToEat
+
+
+	// req.body is the contents of the form that we want to put in the 
+	// db
+	const fruitDoc = await FruitModel.create(req.body)
+	console.log(fruitDoc)
+	res.redirect('/') // tell the client to make a get
+	// request to / 
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const app = express()
 // import the model to talk to the db
 const FruitModel = require('./models/fruit')
 
+const fruitCtrl = require('./controllers/fruits')
+
 // Connect our express server to the database!
 mongoose.connect(process.env.MONGODB_URI)
 
@@ -26,68 +28,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev")); //new
 app.use(methodOverride("_method")); // 
 
+app.use('/fruits', fruitCtrl)
+
 app.get('/', function(req, res){
 	res.render('index.ejs')
 })
 
-app.delete('/fruits/:fruitId', async function(req, res){
-
-	const deletedFruit = await FruitModel.findByIdAndDelete(req.params.fruitId)
-
-	res.redirect('/fruits')
-})
-
-
-app.get('/fruits', async function(req, res){
-	// get all of the fruits from the db!
-
-	const allFruitDocs = await FruitModel.find({})
-	console.log(allFruitDocs)
-
-	res.render('fruits/index.ejs', {fruitDocs: allFruitDocs})
-})
-
-
-app.get('/fruits/new', function(req, res){
-
-	// render always looks in the views folder
-	// for the our ejs filesres.render('fruits/new.ejs')!
-	res.render('fruits/new.ejs')
-})
-
-// the new route must be defined before the show, because params are catch alls
-app.get('/fruits/:fruitId', async function(req, res){
-	console.log(req.params.fruitId, " <- req.params.fruitId")
-
-	// using the id from the request, 
-	// tell the model to go find that specific fruit from the database!
-	const fruitDoc = await FruitModel.findById(req.params.fruitId)
-	console.log(fruitDoc)
-
-	res.render('fruits/show.ejs', {fruitDoc: fruitDoc})
-})
-
-app.post('/fruits', async function(req, res){
-
-	// to access the contents of the form
-	console.log(req.body, " <- body of our request")
-	if(req.body.isReadyToEat === 'on'){
-		req.body.isReadyToEat = true
-	} else {
-		req.body.isReadyToEat = false
-	}
-
-	// one line of if/else
-	// req.body.isReadyToEat = !!req.body.isReadyToEat
-
-
-	// req.body is the contents of the form that we want to put in the 
-	// db
-	const fruitDoc = await FruitModel.create(req.body)
-	console.log(fruitDoc)
-	res.redirect('/fruits') // tell the client to make a get
-	// request to /fruits 
-})
 
 
 app.listen(3000, function(){

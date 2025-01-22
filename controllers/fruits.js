bb3504c70d7097ab50cd230e8fbe69225b8fa1c4
JimHaff <james.haff@generalassemb.ly>
2025-01-22 08:34:53 -0600

    added index

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const FruitModel = require('../models/fruit')
 
 router.delete('/:fruitId', async function(req, res){
 
@@ -10,20 +10,36 @@ router.delete('/:fruitId', async function(req, res){
 })
 
 
+router.get('/:fruitId/edit', async function(req, res){
+	const foundFruit = await FruitModel.findById(req.params.fruitId);
+	console.log(foundFruit);
+	res.render('fruits/edit.ejs', {fruitDoc: foundFruit})
+})
+
+router.put('/:fruitId', async function(req, res){
+	// make sure that our checkbox is changed to a boolean
+	console.log(req.body)
+	req.body.isReadyToEat = !!req.body.isReadyToEat
+	const foundFruit = await FruitModel.findByIdAndUpdate(req.params.fruitId, req.body, {new: true});
+	console.log(foundFruit)
+	res.redirect(`/fruits/${foundFruit._id}`)
+})
+
+
 router.get('/', async function(req, res){
 	// get all of the  from the db!
 
 	const allFruitDocs = await FruitModel.find({})
 	// console.log(allFruitDocs)
 
-	res.render('/index.ejs', {fruitDocs: allFruitDocs})
+	res.render('fruits/index.ejs', {fruitDocs: allFruitDocs})
 })
 
 router.get('/new', function(req, res){
 
 	// render always looks in the views folder
 	// for the our ejs filesres.render('/new.ejs')!
-	res.render('/new.ejs')
+	res.render('fruits/new.ejs')
 })
 
 
@@ -36,7 +52,7 @@ router.get('/:fruitId', async function(req, res){
 	const fruitDoc = await FruitModel.findById(req.params.fruitId)
 	console.log(fruitDoc)
 
-	res.render('/show.ejs', {fruitDoc: fruitDoc})
+	res.render('fruits/show.ejs', {fruitDoc: fruitDoc})
 })
 
 
